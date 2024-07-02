7afcb133176fbd75e45c77decd5016c1ffea4eef
wkdrjsrb <163004929+wkdrjsrb@users.noreply.github.com>
2024-07-02 18:30:22 +0900

    Add files via upload

---
+++ LonbExample.java
@@ -0,0 +1,16 @@
+package sec02.exam03;
+
+public class LonbExample {
+
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		long var3 = 200000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+
+	}
+
+}
