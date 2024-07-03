90cd75abaed39852711e7e82c937656cd02939a4
wkdrjsrb <163004929+wkdrjsrb@users.noreply.github.com>
2024-07-03 22:32:00 +0900

    Add files via upload

---
+++ ByteOperationExample.java
@@ -0,0 +1,18 @@
+package sec03.exam03;
+
+public class ByteOperationExample {
+
+	public static void main(String[] args) {
+		byte result1 = 10 + 20;
+		System.out.println(result1);
+		
+		byte x = 10;
+		byte y = 20;
+		int result = x + y;
+		
+		byte x2 = 30;
+		long y2 = 40;
+		long resutl2 = x2 + y2;
+	}
+
+}
